90988b49c635164dd276b38aa94df3c47f66c4de
Khoa Pham <onmyway133@gmail.com>
2017-11-16 17:42:23 +0100

    Add AppDelegateLifeCycle and ViewControllerLifeCycle

--- Sources/AppDelegateLifeCycle.swift
+++ Sources/AppDelegateLifeCycle.swift
@@ -1,9 +1,87 @@
-//
-//  AppDelegateLifeCycle.swift
-//  RxLifeCycle
-//
-//  Created by Khoa Pham on 16.11.2017.
-//  Copyright © 2017 Khoa Pham. All rights reserved.
-//
+import UIKit
+import RxSwift
 
-import Foundation
+public class AppDelegateLifeCycle {
+  public var didBecomeActive: Observable<Void> {
+    return observer.didBecomeActiveSubject
+  }
+
+  public var willResignActive: Observable<Void> {
+    return observer.willResignActiveSubject
+  }
+
+  public var didEnterBackground: Observable<Void> {
+    return observer.didEnterBackgroundSubject
+  }
+
+  public var willEnterForeground: Observable<Void> {
+    return observer.willEnterForegroundSubject
+  }
+
+  private let observer: Observer
+
+  public init() {
+    observer = Observer()
+  }
+}
+
+private extension AppDelegateLifeCycle {
+  class Observer {
+    let didBecomeActiveSubject = PublishSubject<Void>()
+    let willResignActiveSubject = PublishSubject<Void>()
+    let didEnterBackgroundSubject = PublishSubject<Void>()
+    let willEnterForegroundSubject = PublishSubject<Void>()
+
+    var observers: [AnyObject] = []
+
+    init() {
+      let center = NotificationCenter.default
+      let application = UIApplication.shared
+      let queue = OperationQueue.main
+
+      observers.append(center.addObserver(
+        forName: Notification.Name.UIApplicationDidBecomeActive,
+        object: application,
+        queue: queue,
+        using: { [weak self] _ in
+          self?.didBecomeActiveSubject.onNext(())
+        }
+      ))
+
+      observers.append(center.addObserver(
+        forName: Notification.Name.UIApplicationWillResignActive,
+        object: application,
+        queue: queue,
+        using: { [weak self] _ in
+          self?.willResignActiveSubject.onNext(())
+        }
+      ))
+
+      observers.append(center.addObserver(
+        forName: Notification.Name.UIApplicationDidEnterBackground,
+        object: application,
+        queue: queue,
+        using: { [weak self] _ in
+          self?.didEnterBackgroundSubject.onNext(())
+        }
+      ))
+
+      observers.append(center.addObserver(
+        forName: Notification.Name.UIApplicationWillEnterForeground,
+        object: application,
+        queue: queue,
+        using: { [weak self] _ in
+          self?.willEnterForegroundSubject.onNext(())
+        }
+      ))
+    }
+
+    deinit {
+      observers.forEach {
+        NotificationCenter.default.removeObserver($0)
+      }
+
+      observers.removeAll()
+    }
+  }
+}
--- Sources/ViewControllerLifeCycle.swift
+++ Sources/ViewControllerLifeCycle.swift
@@ -1,9 +1,77 @@
-//
-//  ViewControllerLifeCycle.swift
-//  RxLifeCycle
-//
-//  Created by Khoa Pham on 16.11.2017.
-//  Copyright © 2017 Khoa Pham. All rights reserved.
-//
+import UIKit
+import RxSwift
 
-import Foundation
+public class ViewControllerLifeCycle {
+  public var viewDidLoad: Observable<Void> {
+    return dummyViewController.viewDidLoadSubject
+  }
+
+  public var viewWillAppear: Observable<Void> {
+    return dummyViewController.viewWillAppearSubject
+  }
+
+  public var viewDidAppear: Observable<Void> {
+    return dummyViewController.viewDidAppearSubject
+  }
+
+  public var viewWillDisappear: Observable<Void> {
+    return dummyViewController.viewWillDisappearSubject
+  }
+
+  public var viewDidDisappear: Observable<Void> {
+    return dummyViewController.viewDidDisappearSubject
+  }
+
+  private let dummyViewController: DummyViewController
+
+  public init(viewController: UIViewController) {
+    dummyViewController = DummyViewController()
+
+    // Add
+    viewController.addChildViewController(dummyViewController)
+    viewController.view.addSubview(dummyViewController.view)
+    dummyViewController.view.frame = .zero
+    dummyViewController.view.autoresizingMask = []
+    dummyViewController.didMove(toParentViewController: viewController)
+  }
+}
+
+private extension ViewControllerLifeCycle {
+  class DummyViewController: UIViewController {
+    let viewDidLoadSubject = PublishSubject<Void>()
+    let viewWillAppearSubject = PublishSubject<Void>()
+    let viewDidAppearSubject = PublishSubject<Void>()
+    let viewWillDisappearSubject = PublishSubject<Void>()
+    let viewDidDisappearSubject = PublishSubject<Void>()
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      viewDidLoadSubject.onNext(())
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+
+      viewWillAppearSubject.onNext(())
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+
+      viewDidAppearSubject.onNext(())
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+
+      viewWillDisappearSubject.onNext(())
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+
+      viewDidDisappearSubject.onNext(())
+    }
+  }
+}
